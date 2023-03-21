e5424028676eb0ede599444da884e9017df628d8
Nir Magnezi <nmagnezi@redhat.com>
2023-03-21 19:52:28 +0200

    ISO2: fixing skopeo copy path

--- iso2_agent_data.sh
+++ iso2_agent_data.sh
@@ -29,12 +29,12 @@ function iso2_generator_main() {
   log_info "${func_name}" "Copy butane binary to $POC_DIR/assets/bin/"
   cp "$(which butane)" "$POC_DIR"/assets/bin
 
-  log_info "${func_name}" "Copy (via skopeo) local registry: $REGISTRY_IMAGE to $POC_DIR/images/registry.tar:registry:2"
-  skopeo copy docker://"$REGISTRY_IMAGE" docker-archive:"$POC_DIR"/images/registry.tar:registry:2
-  log_info "${func_name}" "Copy (via skopeo) assisted-installer-agent: $ASSISTED_INSTALLER_AGENT_IMAGE to $POC_DIR/images/ose-agent-installer-node-agent"
-  skopeo copy --all docker://"$ASSISTED_INSTALLER_AGENT_IMAGE" dir:"$POC_DIR"/images/ose-agent-installer-node-agent
-  log_info "${func_name}" "Copy (via skopeo) assisted-service: $ASSISTED_INSTALLER_AGENT_IMAGE to $POC_DIR/images/ose-agent-installer-api-server"
-  skopeo copy --all docker://"$ASSISTED_SERVICE_IMAGE" dir:"$POC_DIR"/images/ose-agent-installer-api-server
+  log_info "${func_name}" "Copy (via skopeo) local registry: $REGISTRY_IMAGE to $POC_DIR/assets/images/registry.tar:registry:2"
+  skopeo copy docker://"$REGISTRY_IMAGE" docker-archive:"$POC_DIR"/assets/images/registry.tar:registry:2
+  log_info "${func_name}" "Copy (via skopeo) assisted-installer-agent: $ASSISTED_INSTALLER_AGENT_IMAGE to $POC_DIR/assets/images/ose-agent-installer-node-agent"
+  skopeo copy --all docker://"$ASSISTED_INSTALLER_AGENT_IMAGE" dir:"$POC_DIR"/assets/images/ose-agent-installer-node-agent
+  log_info "${func_name}" "Copy (via skopeo) assisted-service: $ASSISTED_INSTALLER_AGENT_IMAGE to $POC_DIR/assets/images/ose-agent-installer-api-server"
+  skopeo copy --all docker://"$ASSISTED_SERVICE_IMAGE" dir:"$POC_DIR"/assets/images/ose-agent-installer-api-server
 }
 
 log_info iso1_generator "iso2_generator start"
