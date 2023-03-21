887d458363d3c6cdf0ed67b8be078af77ca08d00
Nir Magnezi <nmagnezi@redhat.com>
2023-03-21 19:05:05 +0200

    ISO2: initial commands, load images with skopeo

--- appliance_config.sh
+++ appliance_config.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 export POC_DIR='/root/appliance-poc-assets'
+export REGISTRY_IMAGE='quay.io/libpod/registry:2.8'
+export ASSISTED_INSTALLER_AGENT_IMAGE='quay.io/masayag/assisted-installer-agent:billi'
+export ASSISTED_SERVICE_IMAGE='quay.io/nmagnezi/assisted-service:appliance2'
+
 
 function log() {
   echo "$(date '+%F %T') $2[$$]: level=$1 msg=\"$3\""
---
+++ iso2_agent_data.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# ISO 2 image for factory: agent.data.iso (not bootable)
+# ======================================================
+# Contains the following assets:
+# - mirror_seq1_000000.tar (entire release payload)
+# - images/registry.tar (to run a local registry)
+# - custom service image
+# - custom installer-agent image
+
+source appliance_config.sh
+
+
+function iso2_generator_main() {
+  func_name=${FUNCNAME[0]}
+
+  log_info "${func_name}" "Creating folder: $POC_DIR/assets/bin"
+  mkdir -p "$POC_DIR"/assets/bin
+
+  log_info "${func_name}" "Creating folder: $POC_DIR/assets/images"
+  mkdir -p "$POC_DIR"/assets/images
+
+  log_info "${func_name}" "Creating folder: $POC_DIR/assets/oc-mirror"
+  mkdir -p "$POC_DIR"/assets/oc-mirror
+
+  log_info "${func_name}" "Copy oc-mirror binary to $POC_DIR/assets/bin/"
+  cp "$(which oc-mirror)" "$POC_DIR"/assets/bin/
+
+  log_info "${func_name}" "Copy butane binary to $POC_DIR/assets/bin/"
+  cp "$(which butane)" "$POC_DIR"/assets/bin
+
+  log_info "${func_name}" "Copy (via skopeo) local registry: $REGISTRY_IMAGE to $POC_DIR/images/registry.tar:registry:2"
+  skopeo copy docker://"$REGISTRY_IMAGE" docker-archive:"$POC_DIR"/images/registry.tar:registry:2
+  log_info "${func_name}" "Copy (via skopeo) assisted-installer-agent: $ASSISTED_INSTALLER_AGENT_IMAGE to $POC_DIR/images/ose-agent-installer-node-agent"
+  skopeo copy --all docker://"$ASSISTED_INSTALLER_AGENT_IMAGE" dir:"$POC_DIR"/images/ose-agent-installer-node-agent
+  log_info "${func_name}" "Copy (via skopeo) assisted-service: $ASSISTED_INSTALLER_AGENT_IMAGE to $POC_DIR/images/ose-agent-installer-api-server"
+  skopeo copy --all docker://"$ASSISTED_SERVICE_IMAGE" dir:"$POC_DIR"/images/ose-agent-installer-api-server
+}
+
+log_info iso1_generator "iso2_generator start"
+iso2_generator_main
+log_info iso1_generator "iso2_generator end"
