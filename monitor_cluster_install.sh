d396c8ad62a5f54f04144ec7fd2c176d5cd27e9f
Nir Magnezi <nmagnezi@redhat.com>
2023-03-22 19:21:45 +0200

    Adds a helper script to monitor the installation

---
+++ monitor_cluster_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source appliance_config.sh
+
+pushd "$POC_DIR"/appliance || exit 1
+
+log_info install_monitor "To run cluster oc commands, ssh into the machine and: export KUBECONFIG=/etc/kubernetes/bootstrap-secrets/kubeconfig"
+
+openshift-install agent wait-for install-completea
