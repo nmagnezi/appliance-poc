fa7f9923c1aba9a359b18984ae5cfa115bce0dca
Daniel Erez <danielerez@gmail.com>
2023-03-22 17:30:04 +0200

    Support env vars in appliance_config

--- appliance_config.sh
+++ appliance_config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export POC_DIR=$HOME/appliance-poc-assets
-export REGISTRY_IMAGE='quay.io/libpod/registry:2.8'
-export ASSISTED_INSTALLER_AGENT_IMAGE='quay.io/masayag/assisted-installer-agent:billi'
-export ASSISTED_SERVICE_IMAGE='quay.io/nmagnezi/assisted-service:appliance2'
-export OCP_INSTALLER_REPO='https://github.com/danielerez/installer.git'
-export OCP_INSTALLER_BRANCH='appliance'
+export POC_DIR=${POC_DIR:-$HOME/appliance-poc-assets}
+export REGISTRY_IMAGE=${REGISTRY_IMAGE:-'quay.io/libpod/registry:2.8'}
+export ASSISTED_INSTALLER_AGENT_IMAGE=${ASSISTED_INSTALLER_AGENT_IMAGE:-'quay.io/masayag/assisted-installer-agent:billi'}
+export ASSISTED_SERVICE_IMAGE=${ASSISTED_SERVICE_IMAGE:-'quay.io/nmagnezi/assisted-service:appliance2'}
+export OCP_INSTALLER_REPO=${OCP_INSTALLER_REPO:-'https://github.com/danielerez/installer.git'}
+export OCP_INSTALLER_BRANCH=${OCP_INSTALLER_BRANCH:-'appliance'}
 export SSH_PUB_KEY=$(cat $HOME/.ssh/id_rsa.pub)
 export PULL_SECRET=$(cat $HOME/pull-secret)
 
