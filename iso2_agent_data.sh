96ca10de188b1d2eab59658b5aa71d8628d826ee
Nir Magnezi <nmagnezi@redhat.com>
2023-03-22 09:37:16 +0200

    ISO2: add missing oc-mirror

--- iso2_agent_data.sh
+++ iso2_agent_data.sh
@@ -53,6 +53,7 @@ function iso2_generator_main() {
   echo "${imageset_config}" > "$POC_DIR"/assets/imageset-config.yaml
   pushd "$POC_DIR" || exit 1
   pushd assets/ || exit 1
+  oc-mirror --config imageset-config.yaml file://archives
   log_info "${func_name}" "Copy mirror_seq1_000000.tar to oc-mirror directory"
   cp archives/mirror_seq1_000000.tar ./oc-mirror
   popd || exit 1
